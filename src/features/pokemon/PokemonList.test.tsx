c4fb5c7d6b7e4b2bb596c7c22dbe728a9f7874b9
Tejas Malani <tejasm08@gmail.com>
2026-03-27 17:06:42 +0530

    test: add test for rendering pokemon data from API

--- src/features/pokemon/PokemonList.test.tsx
+++ src/features/pokemon/PokemonList.test.tsx
@@ -1,8 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { PokemonList } from "./PokemonList";
 import { expect, test } from "vitest";
 
 test("renders pokemon list after loading", () => {
     render(<PokemonList />);
     expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+});
+
+test("renders pokemon data from API", async () => {
+    render(<PokemonList />);
+
+    await waitFor(() => {
+        expect(screen.getByText(/bulbasaur/i)).toBeInTheDocument();
+    });
 });
