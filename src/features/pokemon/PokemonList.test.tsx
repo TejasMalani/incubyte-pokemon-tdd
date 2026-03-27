d2bf437ac56287b4660de4961d695086f349d8dc
Tejas Malani <tejasm08@gmail.com>
2026-03-27 16:57:32 +0530

    "test: add test failed for pokemon list after loading"

--- src/features/pokemon/PokemonList.test.tsx
+++ src/features/pokemon/PokemonList.test.tsx
@@ -6,3 +6,8 @@ test("renders loading state initially", () => {
     render(<PokemonList />);
     expect(screen.getByText(/loading/i)).toBeInTheDocument();
 });
+
+test("renders pokemon list after loading", () => {
+    render(<PokemonList />);
+    expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+});
