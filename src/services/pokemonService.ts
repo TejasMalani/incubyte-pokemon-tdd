3501519652df06204eb48e68e14392ea6303ae1f
Tejas Malani <tejasm08@gmail.com>
2026-03-27 17:12:13 +0530

    test: add test for rendering pokemon data from API

---
+++ src/services/pokemonService.ts
@@ -0,0 +1,6 @@
+// src/services/pokemonService.ts
+export async function fetchPokemonList() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const data = await res.json();
+    return data.results;
+}
